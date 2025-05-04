d481e5d080edfed31c330b02f735e613fb9007de
geet_rh <geetansh335@gmail.com>
2025-05-04 11:32:55 +0530

    responsive slider

--- src/Components/home/Slider.jsx
+++ src/Components/home/Slider.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-export default function Silder() {
+export default function Slider() {
   return (
-    <div className='w-screen z-0 mt-[5.1rem]' style={{ height: "70vh" }}>
+    <div className='w-full z-0 mt-[5.1rem] h-[50vh] sm:h-[60vh] md:h-[70vh]'>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -22,19 +21,34 @@ export default function Silder() {
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper w-full h-full"
-
       >
         <SwiperSlide className='w-full h-full bg-black'>
-          <img alt='slider1' src='https://IPA-Images.b-cdn.net/Assets/slider/slider1.png' className='object-cover' />
+          <img
+            alt='slider1'
+            src='https://IPA-Images.b-cdn.net/Assets/slider/slider1.png'
+            className='w-full h-full object-cover'
+          />
         </SwiperSlide>
         <SwiperSlide className='w-full h-full bg-black'>
-          <img alt='slider2' src='https://IPA-Images.b-cdn.net/Assets/slider/slider2.png' className='object-cover' />
+          <img
+            alt='slider2'
+            src='https://IPA-Images.b-cdn.net/Assets/slider/slider2.png'
+            className='w-full h-full object-cover'
+          />
         </SwiperSlide>
         <SwiperSlide className='w-full h-full bg-black'>
-          <img alt='slider3' src='https://IPA-Images.b-cdn.net/Assets/slider/slider3.png' className='object-cover'/>
+          <img
+            alt='slider3'
+            src='https://IPA-Images.b-cdn.net/Assets/slider/slider3.png'
+            className='w-full h-full object-cover'
+          />
         </SwiperSlide>
-        <SwiperSlide className='w-full h-full bg-black flex justify-center'>
-          <img alt='slider4' src='https://IPA-Images.b-cdn.net/Assets/slider/slider4.png' className='object-contain'/>
+        <SwiperSlide className='w-full h-full bg-black flex justify-center items-center'>
+          <img
+            alt='slider4'
+            src='https://IPA-Images.b-cdn.net/Assets/slider/slider4.png'
+            className='max-w-full max-h-full object-contain'
+          />
         </SwiperSlide>
       </Swiper>
     </div>
